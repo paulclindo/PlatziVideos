bcb88cd6c839321bdf871c4d5c83e4e58ba9bf58
Paul Ccari <paulclindo@gmail.com>
2019-09-15 14:35:10 -0500

    ESlint

---
+++ src/components/HelloWorld.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "../assets/styles/App.scss";
+
+// const HelloWorld = () => <h1>Hi World</h1>;
+const HelloWorld = () => {
+  return <h1>Hola</h1>;
+};
+
+export default HelloWorld;
