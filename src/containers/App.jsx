503d22c8013c393ee2fb439385051bf1556b13fd
Paul Ccari <paulclindo@gmail.com>
2019-09-15 22:59:28 -0500

    Added Sass loader

---
+++ src/containers/App.jsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+import Header from "../components/Header";
+import Search from "../components/Search";
+import "../assets/styles/App.scss";
+import Categories from "../components/Categories";
+import Carousel from "../components/Carousel";
+import CarouselItem from "./../components/CarouselItem";
+import Footer from "../components/Footer";
+
+//Call APIs
+import useInitialState from "./../hooks/useInitialState";
+
+const API = "http://localhost:3000/initalState";
+
+const App = () => {
+  const initialState = useInitialState(API);
+  return initialState.length === 0 ? (
+    <h1>Loading...</h1>
+  ) : (
+    <div className="App">
+      <Header />
+      <Search />
+      {initialState.mylist.length > 0 && (
+        <Categories title="Mi lista">
+          <Carousel>
+            {initialState.mylist.map(item => (
+              <CarouselItem key={item.id} {...item} />
+            ))}
+          </Carousel>
+        </Categories>
+      )}
+
+      <Categories title="Tendencias">
+        <Carousel>
+          {initialState.trends.map(item => (
+            <CarouselItem key={item.id} {...item} />
+          ))}
+        </Carousel>
+      </Categories>
+      <Categories title="Originales de Platzi Video">
+        <Carousel>
+          {initialState.originals.map(item => (
+            <CarouselItem key={item.id} {...item} />
+          ))}
+          <CarouselItem />
+        </Carousel>
+      </Categories>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
