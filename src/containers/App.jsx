f0952762cb489f8f2e6ab4fa8819c8e4fd44d4d1
Paul Ccari <paulclindo@gmail.com>
2020-08-22 20:39:23 -0500

    fixed eslint and set fake json server script

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,45 +1,45 @@
-import React, { useState, useEffect } from "react";
-import Header from "../components/Header";
-import Search from "../components/Search";
-import "../assets/styles/App.scss";
-import Categories from "../components/Categories";
-import Carousel from "../components/Carousel";
-import CarouselItem from "./../components/CarouselItem";
-import Footer from "../components/Footer";
+import React, { useState, useEffect } from 'react';
+import Header from '../components/Header';
+import Search from '../components/Search';
+import '../assets/styles/App.scss';
+import Categories from '../components/Categories';
+import Carousel from '../components/Carousel';
+import CarouselItem from '../components/CarouselItem';
+import Footer from '../components/Footer';
 
 //Call APIs
-import useInitialState from "./../hooks/useInitialState";
+import useInitialState from '../hooks/useInitialState';
 
-const API = "http://localhost:3000/initalState";
+const API = 'http://localhost:3000/initalState';
 
 const App = () => {
   const initialState = useInitialState(API);
   return initialState.length === 0 ? (
-    <h1>Loading...</h1>
+    <h3>Loading...</h3>
   ) : (
-    <div className="App">
+    <div className='App'>
       <Header />
       <Search />
       {initialState.mylist.length > 0 && (
-        <Categories title="Mi lista">
+        <Categories title='Mi lista'>
           <Carousel>
-            {initialState.mylist.map(item => (
+            {initialState.mylist.map((item) => (
               <CarouselItem key={item.id} {...item} />
             ))}
           </Carousel>
         </Categories>
       )}
 
-      <Categories title="Tendencias">
+      <Categories title='Tendencias'>
         <Carousel>
-          {initialState.trends.map(item => (
+          {initialState.trends.map((item) => (
             <CarouselItem key={item.id} {...item} />
           ))}
         </Carousel>
       </Categories>
-      <Categories title="Originales de Platzi Video">
+      <Categories title='Originales de Platzi Video'>
         <Carousel>
-          {initialState.originals.map(item => (
+          {initialState.originals.map((item) => (
             <CarouselItem key={item.id} {...item} />
           ))}
           <CarouselItem />
--- src/hooks/useInitialState.js
+++ src/hooks/useInitialState.js
@@ -1,12 +1,19 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 
-const useInitialState = API => {
+const useInitialState = (API) => {
   const [videos, setVideos] = useState([]);
 
   useEffect(() => {
-    fetch(API)
-      .then(response => response.json())
-      .then(data => setVideos(data));
+    const fetchVideos = async () => {
+      try {
+        const response = await fetch(API);
+        const data = await response.json();
+        setVideos(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchVideos();
   }, []);
 
   return videos;
